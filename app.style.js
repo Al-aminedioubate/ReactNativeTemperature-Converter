66cf2e204de362247cca949643f01edab1ca1e68
Lamine DIOUBATE <domanilamine96@gmail.com>
2023-09-10 20:21:34 -0400

    Proj reactnative temperature converter app

---
+++ app.style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  workspace: {
+    height: 450,
+    backgroundColor: "blue",
+  },
+});
+
+export { s };
